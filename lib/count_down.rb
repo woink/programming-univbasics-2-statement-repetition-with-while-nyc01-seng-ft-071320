3ba80244250836c99cfc7c5ea1ff6329e08d14c7
Ward Price <wardprice@gmail.com>
2020-06-25 12:42:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,6 @@
 countdown_number = 10
 while countdown_number >= 0 do
-  if countdown_number == 10
-    puts "Happy New Year!"
-  puts "#{countdown_number}"
+  puts countdown_number
   countdown_number -= 1
 end
+puts "Happy New Year!"
